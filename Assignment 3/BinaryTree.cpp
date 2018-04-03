0f3f459393f19a247e316493dda28e03f8c862f2
Shan <Shanaliq@gmail.com>
2018-04-03 16:28:04 -0400

    rawr

---
+++ Assignment 3/BinaryTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTree.cpp
+//  Assignment 3
+//
+//  Created by Shan Qamruddin on 4/3/18.
+//  Copyright © 2018 Shan Qamruddin. All rights reserved.
+//
+
+#include "BinaryTree.hpp"
---
+++ Assignment 3/BinaryTree.h
@@ -0,0 +1,29 @@
+//
+//  BinaryTree.hpp
+//  Assignment 3
+//
+//  Created by Shan Qamruddin on 4/3/18.
+//  Copyright © 2018 Shan Qamruddin. All rights reserved.
+//
+
+#ifndef BinaryTree_hpp
+#define BinaryTree_hpp
+
+#include "ItemType.h"
+#include <stdio.h>
+
+#endif /* BinaryTree_hpp */
+
+
+class BinaryTree{
+
+public:
+    BinaryTree();
+    ~BinaryTree();
+    void insert(ItemType &key);
+    
+    
+    
+
+
+}
