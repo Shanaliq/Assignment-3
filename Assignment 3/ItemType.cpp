2ffe7cee79e96c39ef0867ab205c6adc358842ea
Shan Qamruddin <Shanaliq@gmail.com>
2018-04-05 16:06:43 -0400

    rawr

---
+++ Assignment 3/ItemType.cpp
@@ -0,0 +1,26 @@
+//
+//  ItemType.cpp
+//  Project 2
+//
+//  Created by Shan Qamruddin on 3/8/18.
+//  Copyright © 2018 Shan Qamruddin. All rights reserved.
+//
+
+#include "ItemType.h"
+#include <iostream>
+
+
+ItemType::ItemType(){
+}
+
+void ItemType::print(){
+    std::cout << this->value << "\n";
+}
+
+void ItemType::initialize(int number){
+    this->value = number;
+}
+
+int ItemType::getValue() const {
+    return this->value;
+}
